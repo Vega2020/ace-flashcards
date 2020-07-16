7fdf97d0254be21ab327abe3ffa6d7f663e15308
Tucker <tuckerbeauchamp@gmail.com>
2020-07-16 11:00:09 -0700

    setup directory structure

---
+++ db/data.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    title: "html acronym",
+    q: "What does HTML stand for?",
+    a: "Hyper Text Markup Language.",
+  },
+  // copy the format above to enter more questions.
+  { title: "", q: "", a: "" },
+  { title: "", q: "", a: "" },
+];
