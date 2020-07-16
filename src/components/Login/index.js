50fd62c5d7b3a6c8f6916ea60b768a905a169782
Skyler Bluth <webdevelopmentdiva@gmail.com>
2020-07-15 20:09:48 -0700

    login css

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -2,6 +2,9 @@ import React, { useCallback, useContext } from "react";
 import { withRouter, Redirect } from "react-router";
 import app from "../../firebase.js";
 import { AuthContext } from "../../Auth.js";
+import { Button, Form, Container } from 'react-bootstrap';
+import logo from "../../Images/aceLogo.png";
+import "./style.css"
 
 const Login = ({ history }) => {
   const handleLogin = useCallback(
@@ -27,7 +30,29 @@ const Login = ({ history }) => {
   }
 
   return (
-    <div>
+    <div className="bgImg text-center">
+    <img src={logo} className="mt-5 mb-5" width="300px" />
+    <div className="bg-secondary ml-5 mr-5 rounded p-2">
+    {/* New Form Component */}
+    <Form className="text-center" onSubmit={handleLogin} >
+  <Form.Group controlId="formBasicEmail">
+    <h3 className="text-center">Email address</h3>
+    <input className="rounded" size="50" name="email" type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+  <Form.Group controlId="formBasicPassword">
+    <h3 className="text-center">Password</h3>
+    <input className="rounded" size="50" name="password" type="password" placeholder="Password" />
+  </Form.Group>
+  <Button className="p-0" variant="dark" type="submit">
+    Submit
+  </Button>{' '}
+</Form>
+</div>
+    {/* OLD but WORKING Form Component.. */}
+   {/* <div>
       <h1>Log in</h1>
       <form onSubmit={handleLogin}>
         <label>
@@ -38,8 +63,9 @@ const Login = ({ history }) => {
           Password
           <input name="password" type="password" placeholder="Password" />
         </label>
-        <button type="submit">Log in</button>
+        <Button className="p-0" variant="secondary">Log In</Button>{' '}
       </form>
+    </div> */}
     </div>
   );
 };
