f1a2a2a585de070486f3caedbfd78b0a222bb7be
Tucker <tuckerbeauchamp@gmail.com>
2020-07-21 14:12:29 -0700

    add lodash package to react front end

--- client/src/components/cardFrame/index.js
+++ client/src/components/cardFrame/index.js
@@ -1,109 +1,83 @@
-import React, {useState} from "react";
-import "./style.css";
+import React, { useState, useEffect } from "react";
+import uniq from "lodash/uniq";
+import Axios from "axios";
 
+import "./style.css";
 import CardRender from "../cardRender";
 import CardCategory from "../cardCategory";
 
 // import axios from "axios";
 // const axiosCall = axios.get("/all");
 // console.log(axiosCall);
-// adding these lines and then changing all instances of "exampleCardSet" to "axiosCall" was my first attempt - didn't compile that way.
+// adding these lines and then changing all instances of "cards" to "axiosCall" was my first attempt - didn't compile that way.
 
-const exampleCardSet = [
-    {
-        title: "html acronym",
-        q: "What does HTML stand for?",
-        a: "Hyper Text Markup Language",
-        tag: "HTML"
-      },
-      {
-        title: "css acronym",
-        q: "What does CSS stand for?",
-        a: "Cascading Style Sheet",
-        tag: "HTML"
-      },
-      {
-        title: "bork acronym",
-        q: "What does bork stand for?",
-        a: "Hyper Text bork Language",
-        tag: "Broken"
-      },
-      {
-        title: "css acronym",
-        q: "What does broke stand for?",
-        a: "Cascading broke Sheet",
-        tag: "Broken"
-      },
-]
-
-
-
-
-function CardFrame(props)
-{
+function CardFrame(props) {
   //One state for deck, one for the tag to be filtered for.
-  const [card, setCard] = useState(-1);
+  const [cardIndex, setCardIndex] = useState(-1);
   const [deck, setDeck] = useState("Broken");
+  const [cards, setCards] = useState([]);
+
+  // componentDidMount()
+  // TLDR; when the component is rendered on the page
+  useEffect(() => {
+    Axios.get("/all").then(function ({ data }) {
+      console.log(data);
+      setCards(data);
+    });
+  }, []);
 
   //Filter to pick out and set a working deck by tag.
-  const cardSet = exampleCardSet.filter((cardToCheck) => {
+  const cardSet = cards.filter((cardToCheck) => {
     return cardToCheck.tag === deck;
   });
 
   //Little variable to measure the resulting array length, to prevent overshooting.
   const cardMax = cardSet.length - 1;
 
-  
   //This checks to see if the card count is within the range of the deck. If it is, we render the card and navigation.
-  if ( card <= cardMax && card >= 0){
-    return(
+  if (cardIndex <= cardMax && cardIndex >= 0) {
+    return (
+      <div>
         <div>
-        <div>
-        <CardRender front={exampleCardSet[card].q} back={exampleCardSet[card].a} />
+          <CardRender front={cards[cardIndex].q} back={cards[cardIndex].a} />
         </div>
         <div>
-        <button onClick={() => setCard(card - 1)}>Prev</button>
+          <button onClick={() => setCardIndex(cardIndex - 1)}>Prev</button>
         </div>
         <div>
-        <button onClick={() => setCard(card + 1)}>Next</button>
+          <button onClick={() => setCardIndex(cardIndex + 1)}>Next</button>
         </div>
-        </div>
-    )
-}
-else{
-  // If the card count is outside the deck range, we either haven't picked a deck yet, or have navigated past the end.
-  //As such, render the menu.
-
-  //Since sets don't allow duplicate entries, feeding the tag of each item into a set gives us a list of tags.
-  let setOfTags = new Set(exampleCardSet.map(category => (category.tag)));
-  //We turn it back into an array, so we can iterate over it more easily.
-  let arrayOfTags = Array.from(setOfTags);
-  //Then we need to have it return a button for each category.
-  return(
-    <>
-    <div>
-      {
-        arrayOfTags.map(category => (
-          <CardCategory
-            name={category}
-            startDeck={startDeck}
-          />
-          ))
-      }
       </div>
+    );
+  } else {
+    // If the card count is outside the deck range, we either haven't picked a deck yet, or have navigated past the end.
+    //As such, render the menu.
+
+    //Since sets don't allow duplicate entries, feeding the tag of each item into a set gives us a list of tags.
+    let setOfTags = new Set(cards.map((category) => category.tag));
+    //We turn it back into an array, so we can iterate over it more easily.
+    // let arrayOfTags = Array.from(setOfTags);
+    let arrayOfTags = uniq(cards.map((category) => category.tag));
+    //Then we need to have it return a button for each category.
+    return (
+      <>
+        <div>
+          {arrayOfTags.map((category) => (
+            <CardCategory name={category} startDeck={startDeck} />
+          ))}
+        </div>
       </>
-  );
-}
+    );
+  }
 
-//This should get passed to and called by the cardCategories component, and should bring up a deck for use.
-function startDeck(deckName){
-  console.log("Starting a deck:", deckName);
-  setCard(0);
-  setDeck(deckName);
-  console.log("Deck is now:");
-  console.log(deck);
+  //This should get passed to and called by the cardCategories component, and should bring up a deck for use.
+  function startDeck(deckName) {
+    console.log("Starting a deck:", deckName);
+    setCardIndex(0);
+    setDeck(deckName);
+    console.log("Deck is now:");
+    console.log(deck);
+  }
 }
-}
-
 
 export default CardFrame;
